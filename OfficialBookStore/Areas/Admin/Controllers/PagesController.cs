0efc1d19d8b4ca937a43d9915616deb4599a5fad
Olivia Xaysombath <Olivia Xaysombath>
2017-03-08 10:10:49 -0600

    added functionality for admin to add pages to dashboard, success

--- OfficialBookStore/Areas/Admin/Controllers/PagesController.cs
+++ OfficialBookStore/Areas/Admin/Controllers/PagesController.cs
@@ -35,5 +35,61 @@ namespace OfficialBookStore.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Declare slug
+                string slug;
+
+                // Init pageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO title
+                dto.Title = model.Title;
+
+                // Check for and set slug if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // Make sure title and slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "The title or slug you entered already exists.");
+                    return View(model);
+                }
+
+                // DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                // Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            // Set TempData message
+            TempData["SM"] = "Successfully added a new page!";
+
+            // Redirect
+            return RedirectToAction("AddPage");
+        }
+
+
     }
 }
