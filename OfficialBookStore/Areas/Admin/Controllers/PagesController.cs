2b5e4fca1ed017530ac67d63125f1014ef8b4cdb
Olivia Xaysombath <Olivia Xaysombath>
2017-03-08 09:33:59 -0600

    created admin area, add page table and changed nav-bar

---
+++ OfficialBookStore/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OfficialBookStore.Models.Data;
+using OfficialBookStore.Models.ViewModels.Pages;
+
+namespace OfficialBookStore.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare a list of PageVM
+            List<PageVM> pagesList;
+
+
+            using (Db db = new Db())
+            {
+                //Init list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+
+            }
+
+            //Return List
+            return View(pagesList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+    }
+}
