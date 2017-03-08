c51467c38e72eb27205275a1fbc111c460529087
Olivia Xaysombath <Olivia Xaysombath>
2017-03-08 12:10:08 -0600

    built-out functionality for details tab, success

--- OfficialBookStore/Areas/Admin/Controllers/PagesController.cs
+++ OfficialBookStore/Areas/Admin/Controllers/PagesController.cs
@@ -177,6 +177,28 @@ namespace OfficialBookStore.Areas.Admin.Controllers
             return RedirectToAction("EditPage");
         }
 
+        public ActionResult PageDetails(int id)
+        {
+            // Declare PageVM
+            PageVM model;
 
+            using (Db db = new Db())
+            {
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // Confirm page exists
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                // Init PageVM
+                model = new PageVM(dto);
+            }
+
+            // Return view with model
+            return View(model);
+        }
     }
 }
