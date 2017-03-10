b9008225eddca1c71c428867be48410185529a61
Olivia Xaysombath <Olivia Xaysombath>
2017-03-09 23:09:56 -0600

    deleted error files, fix problems in index files, cart is displayed

--- OfficialBookStore/Controllers/CartController.cs
+++ OfficialBookStore/Controllers/CartController.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using OfficialBookStore.Models.ViewModels.Cart;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using OfficialBookStore.Views.Cart;
 
 namespace OfficialBookStore.Controllers
 {
@@ -74,7 +74,6 @@ namespace OfficialBookStore.Controllers
             // Return partial view with model
             return PartialView(model);
         }
-
 
     }
 }
--- OfficialBookStore/Models/ViewModels/Cart/CartVM.cs
+++ OfficialBookStore/Models/ViewModels/Cart/CartVM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace OfficialBookStore.Views.Cart
+namespace OfficialBookStore.Models.ViewModels.Cart
 {
     public class CartVM
     {
