e6f91c385724eef5cd57cb72cf3d3c2bbef63681
Olivia Xaysombath <Olivia Xaysombath>
2017-03-11 14:19:31 -0600

    a user can enter their info to sign up and create an account

--- OfficialBookStore/Controllers/AccountController.cs
+++ OfficialBookStore/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OfficialBookStore.Models.Data;
+using OfficialBookStore.Models.ViewModels.Account;
 
 namespace OfficialBookStore.Controllers
 {
@@ -35,5 +37,69 @@ namespace OfficialBookStore.Controllers
         {
             return View("CreateAccount");
         }
+
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+
+            // Check if passwords match
+            if (!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Passwords do not match.");
+                return View("CreateAccount", model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Make sure username is unique
+                if (db.User.Any(x => x.UserName.Equals(model.UserName)))
+                {
+                    ModelState.AddModelError("", "Username " + model.UserName + " is taken.");
+                    model.UserName = "";
+                    return View("CreateAccount", model);
+                }
+
+                // Create userDTO
+                UserDTO userDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    UserName = model.UserName,
+                    Password = model.Password
+                };
+
+                // Add the DTO
+                db.User.Add(userDTO);
+
+                // Save
+                db.SaveChanges();
+
+                // Add to UserRolesDTO
+                int id = userDTO.Id;
+
+                UserRoleDTO userRolesDTO = new UserRoleDTO()
+                {
+                    UserId = id,
+                    RoleId = 2
+                };
+
+                db.UserRole.Add(userRolesDTO);
+                db.SaveChanges();
+            }
+
+            // Create a TempData message
+            TempData["SM"] = "You are now registered and can login.";
+
+            // Redirect
+            return Redirect("~/account/login");
+        }
+
     }
 }
